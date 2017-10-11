e808ee94189007512b42854dc36ff3e4bf52a6e3
Danilo <danilo.hbru@gmail.com>
2017-10-11 06:35:23 -0400

    Adding format

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io')
 
 const {generateMessage} = require('./utils/message');
+const {generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -21,20 +22,15 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server');
-
-    // io.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
-
+    callback();
+  });
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+  });
 
    socket.on('disconnect', () => {
      console.log('Disconnected from client');
    });
-
-  });
 });
 
 server.listen(port, () => {
